a0e9437bca561ef6885e13df18aa044b13138b1a
Firomsa Mahamud <MFiromsa@gmail.com>
2022-04-05 17:06:51 -0500

    Add attempt solution008.js

--- js/solution008.js
+++ js/solution008.js
@@ -4,4 +4,23 @@
 // Example:
 // 348597 => [7,9,5,8,4,3]
 // 0 => [0]
-// dfadf
+// dfa
+
+// P: Are you ever given an empty array or undefined? What then? Are you always given digits?
+// R: Whatever number was given, it is now split up into an array and the order of the numbers is reversed.
+// E: if we were given 9238 => [8,3,2,9]
+//                     330 => [0,3,3]
+
+
+function digitize(n) {
+    //make number a string and split that number
+    let numArray = n.toString().split('').reverse()
+    // reverse array
+    return Number(...numArray)
+    // make array integers again and return
+}
+
+digitize(9238)
+console.log(digitize(330),[0,3,3])
+
+// can't do it this way, use map
